97d1b61c358a91d517d008a17ed822fe4c1bcb8e
panwarkunal <monupanwar5655@gmail.com>
2024-11-12 22:30:37 +0530

    02greetuser

---
+++ mytypescriptproject/02-greetUser.ts
@@ -0,0 +1,15 @@
+function greet(firstName:string):void{
+    console.log(`hello ,${firstName}`);
+
+
+}
+
+greet("kunal");
+
+
+
+let greetUser =(firstName:string)=>{
+    console.log(`hello ,${firstName}`)
+}
+
+greetUser("kunal");
