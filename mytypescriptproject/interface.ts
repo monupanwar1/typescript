f170a63af167bd8a2a2030f8072762de20d10394
panwarkunal <monupanwar5655@gmail.com>
2024-11-16 21:52:52 +0530

    sum example

--- mytypescriptproject/interface.ts
+++ mytypescriptproject/interface.ts
@@ -1,40 +1,62 @@
-interface UserType{
-    firstName: string,
-    lastName:string,
-    age:number,
-};
-function greet(user:UserType){
-    console.log(`hello ${user.firstName} ${user.lastName} ${user.age}`);
+// interface UserType{
+//     firstName: string,
+//     lastName:string,
+//     age:number,
+// };
+// function greet(user:UserType){
+//     console.log(`hello ${user.firstName} ${user.lastName} ${user.age}`);
+// }
+// let user:UserType={
+//     firstName:"Muskan",
+//     lastName:"Munna",
+//     age:25
+// }
+
+// greet(user);
+
+
+
+// interface Manager{
+//     name:string,
+//     age:number
+// }
+
+
+// interface Employee{
+//     name:string,
+//     department:string
+// }
+
+
+
+// type TeamLeader =Manager & Employee;
+
+// let teamLeader:TeamLeader={
+//     name:"Munna",
+//     age:25,
+//     department:"IT"
+// }
+
+// console.log(teamLeader);
+
+
+
+
+
+
+function greet(name:string):string{
+    return "Hello"+name;
 }
-let user:UserType={
-    firstName:"Muskan",
-    lastName:"Munna",
-    age:25
+
+
+let greeting = greet("kunal")
+console.log(greeting);
+
+
+
+function sum(a:number,b:number):number{
+    return a+b;
 }
 
-greet(user);
-
-
-
-interface Manager{
-    name:string,
-    age:number
-}
-
-
-interface Employee{
-    name:string,
-    department:string
-}
-
-
-
-type TeamLeader =Manager & Employee;
-
-let teamLeader:TeamLeader={
-    name:"Munna",
-    age:25,
-    department:"IT"
-}
-
-console.log(teamLeader);
+let anss = sum(10,20);
+console.log(anss)
